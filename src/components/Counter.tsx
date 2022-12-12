beed9a10db8ffcf739e3163202e2e0ae3f6dc1eb
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-12-12 21:50:53 +0100

    fix import counter component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,8 +1,9 @@
 export const Counter = (props: { title: string; count: number }) => {
-  return (
-    <div>
-      <h5>{props.count}</h5>
-      <p>{props.title}</p>
-    </div>
-  );
-};
+    return (
+      <div>
+        <h5>{props.count}</h5>
+        <p>{props.title}</p>
+      </div>
+    );
+  };
+  
