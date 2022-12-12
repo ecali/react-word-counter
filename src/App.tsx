b324c31cfb6790220b07f3ce130bdbf12623b9d5
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-12-12 20:46:49 +0100

    add template

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+        <div className="wrapper">
+          <div className="container">
+            <textarea id="input-text-area" rows={12} placeholder='Start Typing here...'></textarea>
+          </div>
+          <div className="count">
+            <div className="">
+              <h5>0</h5>
+              <p>Words</p>
+            </div>
+            <div className="">
+              <h5>0</h5>
+              <p>Characters</p>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
